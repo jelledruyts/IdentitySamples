d4a6b0dbf7ac7d13ed5ac52afbc63429b8ebbe2c
Jelle Druyts <jelle_druyts@yahoo.com>
2015-04-30 17:00:47 +0200

    Initial commit containing a web app with only OpenID Connect authentication against Azure AD

---
+++ TodoListWebApp/App_Start/Startup.Auth.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TodoListWebApp
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            var urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = Config.WebAppClientId,
+                    Authority = Config.AadAuthority,
+                    PostLogoutRedirectUri = new Uri(new Uri(Config.RootUrl), urlHelper.Action("SignedOut", "Account")).ToString(),
+                });
+        }
+    }
+}
---
+++ TodoListWebApp/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TodoListWebApp.Startup))]
+
+namespace TodoListWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
