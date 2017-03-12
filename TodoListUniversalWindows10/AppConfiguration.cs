8fc6a02b38970ae04e050ff6738fb3e4d908d9a0
Jelle Druyts <jelle_druyts@yahoo.com>
2017-03-12 11:39:28 +0100

    Cleaned up configuration values

--- TodoListUniversalWindows10/AppConfiguration.cs
+++ TodoListUniversalWindows10/AppConfiguration.cs
@@ -2,17 +2,17 @@
 {
     public static class AppConfiguration
     {
-        public const string TodoListWindows10ClientId = "910d393b-ba70-4205-ad3e-da88d5385e9f";
+        public const string TodoListWindows10ClientId = "";
         public const string TodoListWebApiResourceId = "http://identitysamples/todolistwebapi";
 
         public const string StsRootUrl = "https://login.microsoftonline.com/";
-        public const string StsPath = "identitysamples.onmicrosoft.com";
+        public const string StsPath = "tenant.onmicrosoft.com";
         public const string StsAuthority = StsRootUrl + StsPath;
         // [NOTE] Use the following account provider authority value:
         //   - The full tenant name for one specific Azure AD tenant
         //   - "organizations" for *any* Azure AD tenant, or when using AD FS
         //   - "consumers" for a Microsoft Account
-        public const string AccountProviderAuthority = "https://login.microsoftonline.com/identitysamples.onmicrosoft.com";
+        public const string AccountProviderAuthority = "https://login.microsoftonline.com/tenant.onmicrosoft.com";
         // Authority validation is not supported for AD FS, only for Azure AD.
         public const bool CanValidateAuthority = true;
     }
--- TodoListWebSpa/scripts/appConfig.js
+++ TodoListWebSpa/scripts/appConfig.js
@@ -4,10 +4,10 @@ angular.module('configuration', [])
     {
         StsRootUrl: 'https://login.microsoftonline.com/',
         // The tenant name in Azure AD, or 'adfs' for AD FS (see https://technet.microsoft.com/en-us/windows-server-docs/identity/ad-fs/development/single-page-application-with-ad-fs).
-        StsPath: 'identitysamples.onmicrosoft.com',
+        StsPath: 'tenant.onmicrosoft.com',
         // AD FS does not have a sign-out endpoint, Azure AD does.
         StsSupportsLogOut: true,
-        TodoListWebSpaClientId: '31580c09-e971-431a-8ab7-86a7b244720e',
+        TodoListWebSpaClientId: '',
         TodoListWebApiRootUrl: 'https://localhost:44307/',
         TodoListWebApiResourceId: 'http://identitysamples/todolistwebapi',
         ApplicationName: 'Todo List (SPA)'
